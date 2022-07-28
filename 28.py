86ad44ebdc62d0c9972619a2de23694b28b31b87
JINNY <103866627+yeyae@users.noreply.github.com>
2022-07-28 22:06:52 +0900

    Update 28.py

--- 28.py
+++ 28.py
@@ -8,3 +8,18 @@ pocket=['paper','money','cellphone']
 if 'money' in pocket: pass
 else: print("카드를 꺼내라")
 
+#while문 = 반복문
+prompt = """
+1. Add
+2. Del
+3. List
+4. Quit
+
+Enter number: """
+
+number = 0
+while number != 4:
+print(prompt)
+number = int(input())
+Enter number:
+
