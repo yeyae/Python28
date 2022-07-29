0d1b0f16c46636863f9121407ad4253808e47c6f
JINNY <103866627+yeyae@users.noreply.github.com>
2022-07-29 21:17:38 +0900

    Update 28.py

--- 28.py
+++ 28.py
@@ -14,12 +14,10 @@ prompt = """
 2. Del
 3. List
 4. Quit
-
 Enter number: """
 
 number = 0
 while number != 4:
-print(prompt)
-number = int(input())
-Enter number:
+      print(prompt)
+      #number = int(input()):
 
